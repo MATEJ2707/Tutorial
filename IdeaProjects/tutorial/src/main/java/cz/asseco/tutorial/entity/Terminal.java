1e8813d0d2eb2e54e3171455592ba716ec517efc
KLABENEŠ Martin <martin.klabenes@asseco-ce.com>
2025-03-24 14:39:48 +0100

    INT: uprava entit a dto

--- IdeaProjects/tutorial/src/main/java/cz/asseco/tutorial/entity/Terminal.java
+++ IdeaProjects/tutorial/src/main/java/cz/asseco/tutorial/entity/Terminal.java
@@ -1,25 +1,32 @@
 package cz.asseco.tutorial.entity;
 
+import cz.asseco.tutorial.enums.ModelEnum;
+import cz.asseco.tutorial.enums.StavEnum;
 import jakarta.persistence.*;
 import lombok.Data;
 
-@Entity
+@Entity(name = "TERMINAL")
 @Data
 public class Terminal {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "KOD", nullable = false)
     private int kod;
 
-    @Enumerated(EnumType.STRING)
-    private ModelE model;
-
+    @Column(name = "POZNAMKA")
     private String poznamka;
 
     @Enumerated(EnumType.STRING)
-    private StavT stav;
+    private ModelEnum model;
 
-    @ManyToOne
-    @JoinColumn(name = "pobocka_id")
+    @Enumerated(EnumType.STRING)
+    private StavEnum stav;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "POBOCKA_ID")
     private Pobocka pobocka;
 }
